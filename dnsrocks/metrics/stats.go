d88333473e87380e13809a76d66560711075cc70
Balint Csergo <death0wl@meta.com>
2022-10-19 08:23:56 -0700

    fix memleak related to slidingwindow registration
    Summary:
    we created a new slidingwindow instance for every metric registration to be used as a fallback value with Syncmap.StoreOrLoad. This scheduled same amount of cleaner coroutines leaking counters, which lead to a memleak
    
    Only create a new sliding window  if it did not exist in the sync mqp to resolve this issue
    
    Reviewed By: abulimov
    
    Differential Revision: D40510413
    
    fbshipit-source-id: c19302619dc47736d3a2092f2c0def2029f8246c

--- dnsrocks/metrics/stats.go
+++ dnsrocks/metrics/stats.go
@@ -31,8 +31,10 @@ import (
 // * ResetCounterTo resets the counter to `value`
 // * Get to export them.
 type Stats struct {
-	values  *sync.Map
-	windows *sync.Map
+	vlock   sync.RWMutex
+	wlock   sync.RWMutex
+	values  map[string]int64
+	windows map[string]*slidingWindow
 }
 
 // NewStats creates a new stats counter.
@@ -43,85 +45,86 @@ type Stats struct {
 func NewStats() *Stats {
 	stats := new(Stats)
 
-	stats.values = &sync.Map{}
-	stats.windows = &sync.Map{}
+	stats.values = make(map[string]int64)
+	stats.windows = make(map[string]*slidingWindow)
 	return stats
 }
 
 // IncrementCounter increments the counter for key by 1.
 // Implements dnsserver.IncrementCounter interface.
 func (stats *Stats) IncrementCounter(key string) {
-	val, loaded := stats.values.Load(key)
-	if loaded {
-		intval := val.(int64)
-		stats.values.Store(key, intval+1)
-	} else {
-		stats.values.Store(key, int64(1))
-	}
+	stats.vlock.Lock()
+	stats.values[key]++
+	stats.vlock.Unlock()
 }
 
 // IncrementCounterBy adds Value to the counter for key
 // Implements dnsserver.IncrementCounterBy interface.
 func (stats *Stats) IncrementCounterBy(key string, value int64) {
-	val, loaded := stats.values.Load(key)
-	if loaded {
-		intval := val.(int64)
-		stats.values.Store(key, intval+value)
-	} else {
-		stats.values.Store(key, value)
-	}
+	stats.vlock.Lock()
+	stats.values[key] += value
+	stats.vlock.Unlock()
 }
 
 // ResetCounter sets the counter for key to 0.
 // Implements dnsserver.ResetCounter interface.
 func (stats *Stats) ResetCounter(key string) {
-	stats.values.Store(key, int64(0))
+	stats.vlock.Lock()
+	stats.values[key] = 0
+	stats.vlock.Unlock()
 }
 
 // ResetCounterTo sets the counter for key to the given value.
 // Implements dnsserver.ResetCounterTo interface.
 func (stats *Stats) ResetCounterTo(key string, value int64) {
-	stats.values.Store(key, value)
+	stats.vlock.Lock()
+	stats.values[key] = value
+	stats.vlock.Unlock()
 }
 
 // AddSample adds a sample to the sliding window identified by key
 func (stats *Stats) AddSample(key string, value int64) {
-	newWin, err := newSlidingWindow(60 * time.Second)
-	if err != nil {
-		glog.Errorf("failed to register new sliding window")
+	stats.wlock.Lock()
+	defer stats.wlock.Unlock()
+	var win *slidingWindow
+	winfound, found := stats.windows[key]
+	if !found {
+		newwin, err := newSlidingWindow(60 * time.Second)
+		if err != nil {
+			glog.Errorf("failed to register new sliding window")
+			return
+		}
+		stats.windows[key] = newwin
+		win = newwin
+	} else {
+		win = winfound
 	}
-	win, _ := stats.windows.LoadOrStore(key, newWin)
-	win.(*slidingWindow).Add(value)
+	win.Add(value)
 }
 
 // Get implements export.Int interface
 func (stats *Stats) Get() map[string]int64 {
 	var ret = make(map[string]int64)
-	stats.values.Range(func(key any, value any) bool {
-		kstr := key.(string)
-		val := value.(int64)
-		ret[kstr] = val
-		return true
-	})
-	stats.windows.Range(func(key any, value any) bool {
-		kstr := key.(string)
-		val := value.(*slidingWindow)
+	for key, val := range stats.values {
+		ret[key] = val
+	}
+	for key, val := range stats.windows {
 		samples := val.Samples()
 		sort.Slice(samples, func(i, j int) bool { return samples[i] < samples[j] })
 		if len(samples) > 0 {
-			ret[fmt.Sprintf("%s.min", kstr)] = samples[0]
-			ret[fmt.Sprintf("%s.max", kstr)] = samples[len(samples)-1]
+			ret[fmt.Sprintf("%s.min", key)] = samples[0]
+			ret[fmt.Sprintf("%s.max", key)] = samples[len(samples)-1]
 			var sum int64
 			for _, numb := range samples {
 				sum += numb
 			}
-			ret[fmt.Sprintf("%s.avg", kstr)] = sum / int64(len(samples))
+			ret[fmt.Sprintf("%s.avg", key)] = sum / int64(len(samples))
 		} else {
-			ret[fmt.Sprintf("%s.min", kstr)] = 0
-			ret[fmt.Sprintf("%s.max", kstr)] = 0
-			ret[fmt.Sprintf("%s.avg", kstr)] = 0
+			ret[fmt.Sprintf("%s.min", key)] = 0
+			ret[fmt.Sprintf("%s.max", key)] = 0
+			ret[fmt.Sprintf("%s.avg", key)] = 0
 		}
-		return true
-	})
+	}
+
 	return ret
 }
