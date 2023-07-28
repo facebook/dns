eeca05bfe15acae35f50b31c8d75fa32750129e1
Balint Csergo <deathowlzz@gmail.com>
2023-07-28 11:37:15 +0100

    perftest fbdns on github actions

---
+++ .github/scripts/install_rocks_stable.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo """
+deb http://archive.ubuntu.com/ubuntu/ kinetic main restricted
+deb http://archive.ubuntu.com/ubuntu/ kinetic-updates main restricted
+deb http://archive.ubuntu.com/ubuntu/ kinetic universe
+deb http://archive.ubuntu.com/ubuntu/ kinetic-updates universe
+deb http://archive.ubuntu.com/ubuntu/ kinetic multiverse
+deb http://archive.ubuntu.com/ubuntu/ kinetic-updates multiverse
+""" > /etc/apt/sources.list.d/kinetic.list
+
+apt-get update -qq
+
+apt-get install -qq librocksdb7.3 librocksdb-dev
